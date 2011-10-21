46b9fe93c458badccb21f0ff70284a1c36178d3b
Aaron Zampaglione <azampagl@azampagl.com>
2011-10-21 09:54:46 -0400

    Added skolnick scrapper and data set.

---
+++ data/skolnick/README.txt
@@ -0,0 +1,49 @@
+flavodxin-like
+1b00 d11b00a_
+1dbw d1dbwa_
+1nat d1nata_
+1ntr d1ntra_
+3chy d3chya_
+1qmpa d1qmpa_
+1qmpb d1qmpb_
+1qmpc d1qmpc_
+1qmpd d1qmpd_
+4tmya d4tmya_
+4tmyb d4tmyb_
+
+cupredoxin-like
+1baw d1bawa_
+1byoa d1byoa_
+1byob d1byob_
+1kdi d1kdia_
+1nin d1nina_
+1pla d1plaa_
+2b3i d2b3ia_
+2pcy d2pcya_
+2plt d2plta_
+
+tim_beta-alpha-barrel
+1amk d1amka_
+1aw2 d1aw2a_
+1b9b d1b9ba_
+1btm d1btma_
+1hti d1htia_
+1thm d1thma_
+1tre d1trea_
+1tri d1tria_
+1ydv d1ydva_
+3ypi d3ypia_
+8tim d8tima_
+
+ferritin-like
+1b71 d1b71a_
+1bcf d1bcfa_
+1dps d1dpsa_
+1fha d1fhaa_
+1ier d1iera_
+1rcd d1rcda_
+
+microbial_ribonuclease
+1rn1a d1rn1a_
+1rn1b d1rn1b_
+1rn1c d1rn1c_ 
---
+++ data/skolnick/scrape.py
@@ -0,0 +1,55 @@
+'''
+Scapes PDB database for skolnick data set.
+
+@see http://astral.berkeley.edu/pdbstyle-1.75.html
+@see ./README.txt
+
+@author Aaron Zampaglione <azampagl@my.fit.edu>
+@date 2011-10-27
+'''
+import os
+import sys
+import time
+import urllib
+
+BASE_URL = 'http://astral.berkeley.edu/pdbstyle.cgi?id='
+EXT = 'ent'
+
+# Skolnick dictionary.
+skolnick = {}
+
+#
+# Parse the readme to find fold families and proteins.
+#
+last_fold = ''
+
+readme = open('README.txt', 'r')
+for line in readme:
+    if line == "\n":
+        continue
+    
+    line = line[:-1].split(" ")
+    
+    # Line contains the fold name.
+    if len(line) == 1:
+        last_fold = line[0]
+        skolnick[line[0]] = []
+        continue
+    
+    # Append the protein to the last fold discovered.
+    skolnick[last_fold].append(line[1])
+readme.close()
+
+# Create directories.
+for fold in skolnick.keys():
+    if os.path.exists(fold):
+        os.removedirs(fold)
+    os.mkdir(fold)
+
+for fold, proteins in skolnick.items():
+    for protein in proteins:
+        result = urllib.urlopen(BASE_URL + protein)
+        ent = open(fold + '/' + protein + '.' + EXT, 'w')
+        ent.write(result.read())
+        ent.close()
+        time.sleep(2)
