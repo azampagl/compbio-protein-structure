c2d179b7685208c2ab6d9313af0c239658378d8e
Aaron Zampaglione <azampagl@azampagl.com>
2011-12-30 00:37:50 -0500

    Added local alignment results.

--- src/compbio/anlys/algo/eigas/local_align_anlys.py
+++ src/compbio/anlys/algo/eigas/local_align_anlys.py
@@ -8,30 +8,154 @@ Analysis of hard alignment proteins
 from compbio.algo.eigas.core import EIGAs
 from compbio.algo.eigas.protein.core import Protein
 from compbio.algo.eigas.protein.parser.core import ProteinParser
-from compbio.common.data import MOTIF
+from compbio.common.data import LOCAL
 
 import getopt
+import csv
 import sys
-import pprint
 
 # Get command line args.
-#opts, args = getopt.getopt(sys.argv[1:], ':o')
-#if not len(args):
-#    raise Exception('Missing output file.')
+opts, args = getopt.getopt(sys.argv[1:], ':o')
+if not len(args):
+    raise Exception('Missing output directory.')
 
-protein1 = Protein(ProteinParser.factory('pdb', MOTIF['MICROBIAL-RIBONUCLEASE']['1RDS']))
-#protein2 = Protein(ProteinParser.factory('pdb', MOTIF['MICROBIAL-RIBONUCLEASE']['1BU4']))
-protein2 = Protein(ProteinParser.factory('pdb', MOTIF['MICROBIAL-RIBONUCLEASE']['1RDS']))
+# A specific order is necessary due to the GoTERM analysis by another group (mostly for visual effects).
+ORDER= [
+    '1ADF',
+    '2JHF',
+    '1MGO',
+    '1EE2',
+    '1QV6',
+    '1A71',
+    '1N8K',
+    '1N92',
+    '1P1R',
+    '1YE3',
+    '3BTO',
+    '1QV7',
+    '1KUV',
+    '1KUY',
+    '1KUX',
+    '1L0C',
+    '1IB1e',
+    '1IB1f',
+    '1IB1g',
+    '1IB1h',
+    '1B6B',
+    '1B6Bb',
+    '1B73',
+    '2DWU',
+    '1ZUW',
+    '1B74',
+    '3IST',
+    '2JFU',
+    '2GZM',
+    '3ISV',
+    '3HFR',
+    '2JFV',
+    '2OHO',
+    '1EP0',
+    '1PM7',
+    '1NXM',
+    '2IXK',
+    '1WLT',
+    '1DZR',
+    '1RTV',
+    '2B9U',
+    '1NZC',
+    '1NYW',
+    '1CLK',
+    '1A0C',
+    '1MUW',
+    '1BXB',
+    '2GLK',
+    '1QT1',
+    '1XIM',
+    '1XLA',
+    '1DXI',
+    '2GYI',
+    '1XYL',
+    '1S5N',
+    '1XYA',
+    '3RUBl',
+    '8RUC',
+    '2VDI',
+    '1UZH',
+    '1UWA',
+    '1UPMs',
+    '1SVDm',
+    '1RXOc',
+    '1IWAb',
+    '1IR1s',
+    '1GK8i'
+]
 
-matrix, seqs = EIGAs.local_align(protein1, protein2)
+writer1 = csv.writer(open(args[0] + '/local-align-len.csv', 'wb'))
+writer2 = csv.writer(open(args[0] + '/local-align-len-norm.csv', 'wb'))
+writer3 = csv.writer(open(args[0] + '/local-align-score.csv', 'wb'))
+writer4 = csv.writer(open(args[0] + '/local-align-score-norm.csv', 'wb'))
 
-for seq in [seqs[0]]:
-    print(seq[0].value)
-    print(seq[1])
-    print(seq[2])
-    # print([abs(protein1.fingerprint[i] - protein2.fingerprint[j]) for i, j in zip(seq[1], seq[2])])
-    print('')
-#pprint.pprint(seqs[0])
-#pprint.pprint(seqs[2])
-#pprint.pprint(seqs[30])
+# write our first header
+header = [None]
+header.extend(ORDER)
+writer1.writerow(header)
+writer2.writerow(header)
+writer3.writerow(header)
+writer4.writerow(header)
+
+# Build a result dictionary.
+results1 = {}
+results2 = {}
+results3 = {}
+results4 = {}
+for name1 in ORDER:
+    results1[name1] = {}
+    results2[name1] = {}
+    results3[name1] = {}
+    results4[name1] = {}
+    for name2 in ORDER:
+        results1[name1][name2] = None
+        results2[name1][name2] = None
+        results3[name1][name2] = None
+        results4[name1][name2] = None
+
+# Let's spit out the length of the optimal motif.
+for name1 in ORDER:
+    row1 = [name1]
+    row2 = [name1]
+    row3 = [name1]
+    row4 = [name1]
     
+    for name2 in ORDER:
+        # See if we processed this combination already
+        #if results1[name1][name2] != None:
+        #    continue
+        print(name1 + ' ' + name2)
+        
+        protein1 = Protein(ProteinParser.factory('pdb', LOCAL[name1]))
+        protein2 = Protein(ProteinParser.factory('pdb', LOCAL[name2]))
+        
+        # Norm factor will be the smallest protein fingerprint (which is also
+        #  the largest possible alignment).
+        norm1 = min(len(protein1.fingerprint), len(protein2.fingerprint))
+        # The norm for the second set is the value given when two items 
+        #  match during local alignment (2) and largest possible alignment.
+        norm2 = norm1 * 2
+        
+        # Local align the two proteins.
+        matrix, seqs = EIGAs.local_align(protein1, protein2)
+        
+        results1[name1][name2] = results1[name2][name1] = len(seqs[0][1])
+        results2[name1][name2] = results2[name2][name1] = float(len(seqs[0][1])) / norm1
+        results3[name1][name2] = results3[name2][name1] = seqs[0][0].value
+        results4[name1][name2] = results4[name2][name1] = float(seqs[0][0].value) / norm2
+        
+        row1.append(results1[name1][name2])
+        row2.append(results2[name1][name2])
+        row3.append(results3[name1][name2])
+        row4.append(results4[name1][name2])
+    
+    writer1.writerow(row1)
+    writer2.writerow(row2)
+    writer3.writerow(row3)
+    writer4.writerow(row4)
--- src/compbio/common/data.py
+++ src/compbio/common/data.py
@@ -84,9 +84,49 @@ del SKOLNICK['MICROBIAL_RIBONUCLEASE']['1RN1']
 # Moitf
 #
 MOTIF_EXT = 'pdb'
-MOTIF_DIR = os.path.abspath(DIR + os.sep + 'motif' + os.sep + HARD_EXT + os.sep)
+MOTIF_DIR = os.path.abspath(DIR + os.sep + 'motif' + os.sep + MOTIF_EXT + os.sep)
 MOTIF = {}
 for d in os.listdir(MOTIF_DIR):
     MOTIF[d.upper()] = {}
     for f in os.listdir(MOTIF_DIR + os.sep + d):
         MOTIF[d.upper()][f.rsplit('.', 1)[0]] = (f.rsplit('.', 1)[0], os.path.abspath(MOTIF_DIR + os.sep + d + os.sep + f), None)
+
+
+#
+# Local
+#
+LOCAL_EXT = 'pdb'
+LOCAL_DIR = os.path.abspath(DIR + os.sep + 'local' + os.sep + LOCAL_EXT + os.sep)
+LOCAL = {}
+for f in os.listdir(LOCAL_DIR):
+    LOCAL[f.rsplit('.', 1)[0]] = (f.rsplit('.', 1)[0], os.path.abspath(LOCAL_DIR + os.sep + f), None)
+
+# Special cases for local
+LOCAL['1IB1e'] = ('1IB1e', LOCAL['1IB1'][1], 'E')
+LOCAL['1IB1f'] = ('1IB1f', LOCAL['1IB1'][1], 'F')
+LOCAL['1IB1g'] = ('1IB1g', LOCAL['1IB1'][1], 'G')
+LOCAL['1IB1h'] = ('1IB1h', LOCAL['1IB1'][1], 'H')
+del LOCAL['1IB1']
+
+LOCAL['1B6Bb'] = ('1B6Bb', LOCAL['1B6B'][1], 'B')
+
+LOCAL['3RUBl'] = ('3RUBl', LOCAL['3RUB'][1], 'L')
+del LOCAL['3RUB']
+
+LOCAL['1UPMs'] = ('1UPMs', LOCAL['1UPM'][1], 'S')
+del LOCAL['1UPM']
+
+LOCAL['1SVDm'] = ('1SVDm', LOCAL['1SVD'][1], 'M')
+del LOCAL['1SVD']
+
+LOCAL['1RXOc'] = ('1RXOc', LOCAL['1RXO'][1], 'C')
+del LOCAL['1RXO']
+
+LOCAL['1IWAb'] = ('1IWAb', LOCAL['1IWA'][1], 'B')
+del LOCAL['1IWA']
+
+LOCAL['1IR1s'] = ('1IR1s', LOCAL['1IR1'][1], 'S')
+del LOCAL['1IR1']
+
+LOCAL['1GK8i'] = ('1GK8i', LOCAL['1GK8'][1], 'I')
+del LOCAL['1GK8']
